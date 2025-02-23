283f64cea75a8e321cdae08f54a50da77d7d08b7
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-02-24 02:16:09 +0700

    Add Data Dummy

---
+++ src/components/data/data.js
@@ -0,0 +1,59 @@
+import ktp from "../../assets/ktp.png";
+import kk from "../../assets/kk.png";
+import aktaKelahiran from "../../assets/aktakelahiran.jpg";
+import aktaKematian from "../../assets/aktakematian.jpg";
+import bukuNikah from "../../assets/bukunikah.png";
+import skck from "../../assets/skck.png";
+import suratPindah from "../../assets/suratpindah.png";
+import pbb from "../../assets/pbb.png";
+
+export const data = [
+  {
+    id: 1,
+    title: "KTP",
+    image: ktp,
+    link: "https://ppid.semarangkota.go.id/kb/pelayanan-ktp-el-kota-semarang/",
+  },
+  {
+    id: 2,
+    title: "Kartu Keluarga",
+    image: kk,
+    link: "https://ppid.semarangkota.go.id/kb/pelayanan-kartu-keluarga-kk-kota-semarang/",
+  },
+  {
+    id: 3,
+    title: "Akta Kelahiran",
+    image: aktaKelahiran,
+    link: "https://sidnok.semarangkota.go.id/data-dukung",
+  },
+  {
+    id: 4,
+    title: "Akta Kematian",
+    image: aktaKematian,
+    link: "https://sidnok.semarangkota.go.id/data-dukung",
+  },
+  {
+    id: 5,
+    title: "Buku Nikah",
+    image: bukuNikah,
+    link: "https://ppid.semarangkota.go.id/kb/cara-mendapatkan-surat-nikah/",
+  },
+  {
+    id: 6,
+    title: "SKCK",
+    image: skck,
+    link: "https://ppid.semarangkota.go.id/kb/buat-skck-harus-ke-kantor-polisi-lewat-online-saja/",
+  },
+  {
+    id: 7,
+    title: "Surat Pindah",
+    image: suratPindah,
+    link: "https://sidnok.semarangkota.go.id/data-dukung",
+  },
+  {
+    id: 8,
+    title: "PBB",
+    image: pbb,
+    link: "https://e-pbb.semarangkota.go.id/",
+  },
+];
---
+++ src/components/data/news.js
@@ -0,0 +1,45 @@
+import img from "../../assets/kel_tlogosari.jpg";
+import img2 from "../../assets/example.jpg";
+import img3 from "../../assets/example2.jpg";
+
+export const news = [
+  {
+    id: 1,
+    title: "Pemkot Semarang Respon Cepat Tangani Tanggul Jebol di Kali Plumbon",
+    date: "03/02/2025",
+    time: "12:00",
+    bidang: "Umum",
+    description: `Pemerintah kota atau Pemkot Semarang melalui Dinas Pekerjaan Umum (DPU) bergerak cepat dalam menangani tanggul jebol di Kali Plumbon...`,
+    image: img,
+  },
+  {
+    id: 2,
+    title:
+      "Atasi Genangan di Kali Tenggang, Pemkot Semarang Tambah 2 Unit Pompa Portable",
+    date: "04/02/2025",
+    time: "07:50",
+    bidang: "Sarana Prasarana",
+    description: `Menindaklanjuti keluhan masyarakat terkait tidak berfungsinya pompa Kali Tenggang...`,
+    image: img2,
+  },
+  {
+    id: 3,
+    title:
+      "Genangan Air di Tlogosari Kulon Cepat Surut, Pemkot Semarang Intens Upayakan Mitigasi",
+    date: "05/02/2025",
+    time: "12:00",
+    bidang: "Umum",
+    description: `Kondisi genangan yang sempat terjadi di wilayah Tlogosari Kulon akibat intensitas hujan yang sangat tinggi...`,
+    image: img3,
+  },
+  {
+    id: 4,
+    title:
+      "Pemkot Semarang Gerak Cepat Lakukan Perbaikan Jalan Kota Terdampak Genangan",
+    date: "13/02/2025",
+    time: "08:00",
+    bidang: "Berita",
+    description: `Pemerintah Kota atau Pemkot Semarang melalui Dinas Pekerjaan Umum (DPU) gerak cepat melakukan perbaikan jalan...`,
+    image: img3,
+  },
+];
